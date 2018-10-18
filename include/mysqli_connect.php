5eeeaeb9fda3d65b4db231b9868a8f90ac3b2e9f
thazin thazin <thazinhtun042017@gmail.com>
2018-10-18 08:00:40 +0630

    first commit

---
+++ include/mysqli_connect.php
@@ -0,0 +1,12 @@
+<?php
+
+//db details
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'restapi';
+
+//connect and select db
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+require_once('include/mysqli_connect.php');
+header('content-type:application/json');
+
+$actionName = $_POST["actionName"];
+
+if ($actionName == "selectPost") {
+
+	$query = "SELECT * FROM post ORDER BY id DESC";
+	$result = mysqli_query($conn, $query);
+
+	$rowCount = mysqli_num_rows($result);
+
+	if ($rowCount . 0) {
+		$postData = array();
+		while ($row = mysqli_fetch_assoc($result)) {
+			$postData[] = $row;
+		}
+		$resultData = array('status' => true, 'postData' => $postData);
+	} else {
+		$resultData = array('status' => false, 'message' => 'No Post Found ...');
+	}
+
+	echo json_encode($resultData);
+}
+
+if ($actionName == "insertPost") {
+
+	$postName = isset($_POST["postName"]) ? $_POST["postName"] : '';
+	$postDesc = isset($_POST["postDesc"]) ? $_POST["postDesc"] : '';
+
+	$query = "INSERT INTO post(post_title, post_desc, status) VALUES('$postName', '$postDesc', 0)";
+	$result = mysqli_query($conn, $query);
+
+	if (!empty($postName) && !empty($postDesc)) {
+		if ($result) {
+			$resultData = array('status' => true, 'message' => 'New Post Inserted SUccessfully...');
+		} else {
+			$resultData = array('status' => false, 'message' => 'Cannot new Post Insert ...');
+		}
+	} else {
+		$resultData = array('status' => false, 'message' => 'Please enter post details ...');
+	}
+
+	echo json_encode($resultData);
+}
+
+ ?>
